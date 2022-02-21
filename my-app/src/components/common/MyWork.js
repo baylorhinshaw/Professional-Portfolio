485dd6d2b97979fc9bdede7a2ee2681ab796a4c4
Baylor <baylorhinshaw21@gmail.com>
2022-02-21 18:58:02 -0500

    did a tad bit today

---
+++ my-app/src/components/common/MyWork.js
@@ -0,0 +1,13 @@
+import './MyWork.css';
+import React from "react";
+
+function MyWork() {
+    return (
+        <div>
+            <h2>My Work</h2>
+            
+        </div>
+    );
+}
+
+export default MyWork;
---
+++ my-app/src/components/common/Nav.js
@@ -0,0 +1,10 @@
+import './Nav.css';
+import React from "react";
+
+function Nav() {
+    return (
+        <h1>HOME HOME HOME</h1>
+    );
+}
+
+export default Nav;
