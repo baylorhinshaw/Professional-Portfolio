efe0096a93f4333eb3ee3bd88511f1ba62aa2f48
Baylor Hinshaw <baylorhinshaw@Baylors-MBP.attlocal.net>
2022-02-23 02:01:38 -0500

    worked on footer

--- my-app/src/components/common/Footer.js
+++ my-app/src/components/common/Footer.js
@@ -7,13 +7,15 @@ import Email from '../../images/email-purple.svg';
 function Footer() {
     return (
         <div className='Footer'>
-            <div>Let's Work Together</div>
-            <a href='#'>
-                <div>baylorhinshaw21@gmail.com</div>    
-            </a>
-            <img className='FooterIcon' src={Linkedin}></img>
-            <img className='FooterIcon' src={Github}></img>
-            <img className='FooterIcon' src={Email}></img>
+            <div className='Row'>
+                <div>Let's Work Together</div>
+                <a href='mailto:baylorhinshaw21@gmail.com'>
+                    <div>baylorhinshaw21@gmail.com</div>  
+                </a>  
+                <a href='https://www.linkedin.com/in/baylorhinshaw/'><img className='FooterIcon' src={Linkedin}></img></a>
+                <a href='https://github.com/baylorhinshaw'><img className='FooterIcon' src={Github}></img></a>
+                <a href='mailto:baylorhinshaw21@gmail.com'><img className='FooterIcon' src={Email}></img></a>            
+            </div>
         </div>
     );
 }
