659b83df0fdc725381b7e49810c80bd2a5c9634b
Baylor <baylorhinshaw21@gmail.com>
2022-03-01 21:30:08 -0500

    got all routes working and modal

--- my-app/src/components/project/ProjectJumbotron.js
+++ my-app/src/components/project/ProjectJumbotron.js
@@ -6,7 +6,7 @@ import Email from '../../images/email-white.svg';
 import MenuImage from '../../images/menu.svg';
 import { Link } from 'react-router-dom';
 import Modal from 'react-modal';
-import Menu from '../common/Menu';
+import Close from '../../images/close.svg';
 
 function ProjectJumbotron() {
 
@@ -46,7 +46,35 @@ function ProjectJumbotron() {
                 </div>
             </div>
             <Modal isOpen={modalIsOpen} style={customStyles}>
-                <Menu/>
+                <div className='Menu-Background'>
+                    <div className='menu-header'>
+                        <div className='column-2 jumbo-hover'>
+                            <Link to='/'><h2>Baylor Hinshaw</h2></Link>
+                        </div>
+                        <div className='column-2'>
+                            <div className='project-jumbotron-row'>
+                                <a href='https://www.linkedin.com/in/baylorhinshaw/' target='_blank'><img className='HeaderIcon' src={Linkedin}></img></a>
+                                <a href='https://github.com/baylorhinshaw' target='_blank'><img className='HeaderIcon' src={Github}></img></a>
+                                <a href='mailto:baylorhinshaw21@gmail.com'><img className='HeaderIcon' src={Email}></img></a>
+                                <a onClick={() => setModalIsOpen(false)}><img className='HeaderIcon menu-btn' src={Close}></img></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='menu-row'>
+                        <div className='directory'>
+                            <Link to='/'><strong>Home</strong></Link>
+                            <ul>
+                                <li>
+                                    <Link to='/project1'>Project One</Link>
+                                </li>
+                                <li>
+                                    <Link to='/project2'>Project Two</Link>
+                                </li>
+                            </ul>
+                            <a className='directory-email' href='mailto:baylorhinshaw21@gmail.com'>baylorhinshaw21@gmail.com</a>
+                        </div>
+                    </div>
+                </div>
             </Modal>
         </div>
     );
